14e8b5d4c8c8a82d0002bf2262c3213e2fbd70ae
Jerry <75481387+JerryMaomao343@users.noreply.github.com>
2025-03-30 13:57:34 +0100

    Enum Class

---
+++ NoughtsCrossesGame/Assets/Scripts/Enums.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enums
+{
+    /// <summary>
+    /// 游戏事件
+    /// </summary>
+    public enum GameEvent
+    {
+        OnPlayerSpawn,
+        OnPlayerDeath,
+        OnLevelComplete,
+    }
+
+}
